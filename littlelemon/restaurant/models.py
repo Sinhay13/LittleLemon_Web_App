1fc081b1ab3dcdde1aa65fe97b4b956906115fd3
Yahnis <yahnis.allegret@yahoo.com>
2023-04-11 18:15:53 +0200

    working on models

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Menu(models.Model):
+    ID = models.IntegerField(max_length=5)
+    Title=models.CharField(max_length=255)
+    Price = models.DecimalField(max_digits=10, decimal_places=2)
+    Inventory= models.IntegerField(max_length=5)
