3ece77c13243d9e644f0f75ddce621bb747aaa19
Yahnis <yahnis.allegret@yahoo.com>
2023-04-12 17:55:40 +0200

    API Menu created

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,7 +1,9 @@
 #define URL route for index() view
 from django.urls import path
-from . import views
+from .views import MenuItemsView, SingleMenuItemView
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    #path('', views.index, name='index'),
+    path('menu/', MenuItemsView.as_view()),
+    path('menu/<int:pk>', SingleMenuItemView.as_view()),
 ]
